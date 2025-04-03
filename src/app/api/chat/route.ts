f294256401e908fd627bf2b71d9f4f1a1220f02e
lbr <lbr9932@gmail.com>
2025-04-03 09:05:23 +0900

    ♻️ KEY Name Update

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.TEST_KEY}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
